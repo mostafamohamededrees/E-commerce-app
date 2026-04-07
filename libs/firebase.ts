c41df7f5586e7a44208c1ac291196d67e8733282
mostafa <mostafaelreyadi@gmail.com>
2026-04-07 06:43:32 +0200

    fix delete products in Dashboard

--- libs/firebase.ts
+++
@@ -1,5 +0,0 @@
-// This file has been deprecated and replaced with Supabase Storage
-// See libs/supabase.ts for the new Supabase client initialization
-// All Firebase functionality has been migrated to Supabase Storage
-
-export default {};
