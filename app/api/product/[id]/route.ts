98a0956b72cbe85709f60513f9a41e6d1d3f51df
mostafa <mostafaelreyadi@gmail.com>
2026-04-07 06:02:29 +0200

    fix delete in route ts

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -2,20 +2,23 @@ import { getCurrentUser } from "@/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/libs/prismadb";
 
-export async function DELETE(request: Request, { params }: { params: { id: string } }) {
-    const currentUser = await getCurrentUser();
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const currentUser = await getCurrentUser();
 
-    if(!currentUser) return NextResponse.error();
+  if (!currentUser) return NextResponse.error();
 
-    if ( currentUser.role !== "ADMIN") {
+  if (currentUser.role !== "ADMIN") {
     return NextResponse.error();
-    }
+  }
 
-    const product = await prisma?.product.delete({
-        where: {
-            id: params.id
-        }
-    });
+  const product = await prisma?.product.delete({
+    where: {
+      id: params.id,
+    },
+  });
 
-    return NextResponse.json(product);
+  return NextResponse.json(product);
 }
